f5e1b8284df9354b27215fc045183fa98f2dc58f
Gideon lenkai <gideonlenkai2021@gmail.com>
2025-07-25 05:04:14 +0300

    Add Tailwind config for dark mode support

--- flickster-movies-App/flickster-app/tailwind.config.js
+++ flickster-movies-App/flickster-app/tailwind.config.js
@@ -1,9 +1,13 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-
+}
